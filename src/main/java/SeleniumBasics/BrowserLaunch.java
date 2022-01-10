5ef737adc5cb9958bfcd00024993e07bf44fa50a
santoshkumar9945 <dsantosh9945@gmail.com>
2022-01-10 20:06:58 +0530

    Browser Launch

---
+++ src/main/java/SeleniumBasics/BrowserLaunch.java
@@ -0,0 +1,29 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kaif\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		ChromeDriver driver= new ChromeDriver();
+		
+		driver.get("https://bugasura.io/");
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
