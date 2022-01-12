a26d446d685ec01f0d985f25b03e50a1da0c457e
santoshkumar9945 <dsantosh9945@gmail.com>
2022-01-12 12:11:37 +0530

    updated

--- src/main/java/SeleniumBasics/BrowserLaunch.java
+++ src/main/java/SeleniumBasics/BrowserLaunch.java
@@ -2,64 +2,43 @@ package SeleniumBasics;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserLaunch {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		WebDriver driver;
 
-		
-		
-		String browername= "chrome";
-		
-		if(browername.equals("chrome"))
-		{
-			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kaif\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		String browername = "chrome";
+
+		if (browername.equals("chrome")) {
+			// System.setProperty("webdriver.chrome.driver",
+			// "C:\\Users\\Kaif\\Downloads\\chromedriver_win32\\chromedriver.exe");
 			WebDriverManager.chromedriver().setup();
-		
-		 driver= new ChromeDriver();
-		}if(browername.equals("firefox"))
-		{
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Kaif\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
-			
-			
-			
-			 driver = new FirefoxDriver();
-		}if(browername.equals("edge"))
-		{
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Kaif\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
-			
-			
-			
-			 driver = new FirefoxDriver();
-			
-		}else
-		{
+
+			driver = new ChromeDriver();
+		}
+		if (browername.equals("firefox")) {
+			// System.setProperty("webdriver.gecko.driver",
+			// "C:\\Users\\Kaif\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+
+			driver = new FirefoxDriver();
+		}
+		if (browername.equals("edge")) {
+			//System.setProperty("webdriver.gecko.driver",
+					//"C:\\Users\\Kaif\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+
+			driver = new EdgeDriver();
+
+		} else {
 			System.out.println("please pass the correct browser name");
 		}
-			
-		
-		
-		
-		
-		
+
 //		
-		
-	
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	
 
 }
